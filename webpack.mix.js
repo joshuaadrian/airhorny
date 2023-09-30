cf742771b4bbff5058357033a2a101b510ffc5da
Joshua Adrian <joshua@joshuaadrian.com>
2023-09-29 22:04:09 -0500

    styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,7 @@ const themename = 'airhorny';
 const homedir   = require('os').homedir();
 const domain    = 'airhorny.test';
 
-mix.setPublicPath('dist');
+mix.setPublicPath('/dist');
 mix.setResourceRoot('/');
 
 mix.webpackConfig({
